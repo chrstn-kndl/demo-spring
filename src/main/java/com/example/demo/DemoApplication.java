f4c1030704345daedab466fbe6ee327ec3095db9
christian.kindel <christian.kindel@cgm.com>
2022-12-12 09:30:04 +0100

    Initial commit for spring demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class DemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/example/demo/config/SpringitProperties.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+public class SpringitProperties {
+    /**
+     * This is our welcome message
+     */
+    private String welcomeMsg = "Hello World";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+}
