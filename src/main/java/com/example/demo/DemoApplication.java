096b74aaeab9576f16ccf1267f35a705b23321e7
christian.kindel <christian.kindel@cgm.com>
2023-01-24 10:07:30 +0100

    Setting up out in memory database

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,41 @@
 package com.example.demo;
 
 import com.example.demo.config.SpringitProperties;
+import com.example.demo.domain.Comment;
+import com.example.demo.domain.Link;
+import com.example.demo.repository.CommentRepository;
+import com.example.demo.repository.LinkRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitProperties.class)
+@EnableJpaAuditing
 public class DemoApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
+        return args -> {
+            Link link = new Link("Getting Started with SpringBoot 2", "http://therealdanvega.com/spring-boot-2");
+            linkRepository.save(link);
+
+            Comment comment = new Comment("This Spring Boot 2 link is awesome!", link);
+            commentRepository.save(comment);
+            link.addComment(comment);
+
+            System.out.println("We just inserted a link and a comment");
+            System.out.println("============================================================");
+        };
     }
 }
--- src/main/java/com/example/demo/domain/Link.java
+++ src/main/java/com/example/demo/domain/Link.java
@@ -1,8 +1,6 @@
 package com.example.demo.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,8 +14,10 @@ import java.util.List;
  */
 
 @Entity
+@RequiredArgsConstructor
+@Getter @Setter
+@ToString
 @NoArgsConstructor
-@Data
 public class Link extends Auditable{
 
     @Id
@@ -30,4 +30,8 @@ public class Link extends Auditable{
 
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
 }
